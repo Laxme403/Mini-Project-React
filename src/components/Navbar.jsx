d194741b1be08bb432c5070b4c6fb3296cf2673d
Laxme403 <lakshmirajesh403@gmail.com>
2024-04-18 11:42:35 +0530

    nav bar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <div className="flex px-5 justify-between top-0 bg-black h-20 w-full fixed">
       <div className="flex flex-col">
-      <img src="logonew.svg" alt="FitHeal text" className="w-12"/><p className="font-bold text-white text-2xl ">FitHeal</p>
+      <img src="/logonew.svg" alt="FitHeal text" className="w-12"/><p className="font-bold text-white text-2xl ">FitHeal</p>
       </div>
       <div className="flex">
         <Link to="/">
-          <img className="w-20" src="home.svg" alt="Home" />
+          <img className="w-20" src="/home.svg" alt="Home" />
         </Link>
         <Link to="/login">
-          <img className="w-20" src="login.svg" alt="Person" />
+          <img className="w-20" src="/login.svg" alt="Person" />
         </Link>
       </div>
     </div>
